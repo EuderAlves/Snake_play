238f2a505aee35f3ad0c9a1defd37092253614af
Euder Alves <euder.alv@gmail.com>
2021-04-11 17:48:16 -0300

    jogo completo

--- js/script.js
+++ js/script.js
@@ -48,11 +48,20 @@ function update (event){
 } 
 //inicar as funções para iniciar o jogo
 function iniciarJogo(){
+
 //função para atravessar limete do background e aparecer do outro lado
     if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
     if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
     if(snake[0].y > 15 * box && direction == "up") snake[0].y = 0;
     if(snake[0].y < 0 && direction == "down") snake[0].y = 16 * box;
+    
+//Especificando fim de jogo por cabeça se chocar com corpo
+    for(i =1; i < snake.length; i ++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert("Game over :(");
+        }
+    }    
     //inicar background do jogo
     criarBG();
     //inicar corpo da Snake
